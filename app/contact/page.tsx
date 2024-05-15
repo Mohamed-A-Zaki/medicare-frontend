4f943c0f86ff29cee475688a8362bfbb777897d6
mohamedzaki385 <mohamedzaki385@gmail.com>
2024-05-15 19:40:10 +0000

    create contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,38 @@
+import { Button, Container, TextInput, Textarea, Title } from "@mantine/core";
+
 export default function Contact() {
-  return <div>Contact page</div>;
+  return (
+    <form className="w-[600px] max-w-full mx-auto my-20">
+      <Container>
+        <div className="text-center">
+          <Title order={2} className="mb-7 text-2xl">
+            Contact Us
+          </Title>
+          <p className="text-textColor text-sm">
+            Got a technical issue? want to send feedback about abeta feature?
+            let us know
+          </p>
+        </div>
+
+        <div className="my-5">
+          <TextInput placeholder="example@gmail.com" label="Your Email" />
+        </div>
+
+        <div className="my-5">
+          <TextInput
+            placeholder="let us know how we can help you"
+            label="Subject"
+          />
+        </div>
+
+        <div className="my-5">
+          <Textarea placeholder="Leave a comment" label="Your Message" />
+        </div>
+
+        <Button className="!bg-primaryColor" type="submit">
+          Submit
+        </Button>
+      </Container>
+    </form>
+  );
 }
